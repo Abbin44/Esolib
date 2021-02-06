4215eb501b78b8dc339dd7f4bc75f562301885d8
Abbin44 <abbin44@gmail.com>
2021-02-07 00:46:17 +0100

    All basic tools added

--- Esolib/Esolib.cs
+++ Esolib/Esolib.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Esolib
 {
     public static class Esolib
     {
-        public static char[,] ReadFileToGrid(string filePath, int width, int height, bool printArray)
+        public static char[,] ReadFileToGrid(int width, int height, string filePath, bool printArray)
         {
             ///<Summary>Creates a 2D array from a text file
+            ///</Summary>
             var map = new char[height, width];
             StreamReader sr = new StreamReader(filePath);
 
@@ -46,6 +48,7 @@ namespace Esolib
         public static int[] GetArraySizeFromFile(string filePath)
         {
             ///<Summary>Gets the height and width of your file
+            ///</Summary>
             int[] size = new int[2];
             FileInfo fi = new FileInfo(filePath);
             size[0] = File.ReadLines(fi.FullName).Count();
@@ -60,6 +63,7 @@ namespace Esolib
         public static int[] GetLongestAndShortest(string filePath)
         {
             ///<Summary>Gets the lenght of the shortest and longest lines
+            ///</Summary>
             int[] size = new int[2];
             var lines = File.ReadLines(filePath);
 
@@ -70,5 +74,62 @@ namespace Esolib
 
             return size;
         }
+
+        public static void AddInvisibleBorder(int width, int height, string filePath)
+        {
+            ///<Summary>Adds an invisible border around all code to prenvent checking null values in the 2D array
+            ///</Summary>
+            string[] lines = new string[5];
+            int lineCounter = 0;
+
+            const Int32 BufferSize = 128;
+            using (var fileStream = File.OpenRead(filePath))
+            using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    //Reads file and adds lines to an array
+                    lines[lineCounter] = line;
+                    Console.WriteLine(lines[lineCounter]); //THIS IS THE GOOD LOOKING DEBUG PRINT
+                    lineCounter++;
+                }
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    //Check if a line is non-existant, and make it empty in that case
+                    if (lines[i] == null)
+                        lines[i] = "";
+
+                    //Check if line is less than longest line
+                    if (lines[i].Length < width)
+                    {
+                        int diff = width - lines[i].Length;
+                        char blank = ' ';
+
+                        for (int j = 0; j < diff; j++)
+                        {
+                            //Add a blank space diff ammount of times to the end of the line
+                            lines[i] += blank;
+                        }
+                    }
+                    else
+                        continue;
+                }
+            }
+            //Write edited text to file
+            File.WriteAllLines(filePath, lines);
+        }
+
+        public static bool IsValidFile(string filePath, string extention)
+        {
+            ///<Summary>Checks if the file path is pointing to a valid file type
+            ///</Summary>
+            if (filePath.EndsWith(extention))
+                return true;
+            else
+                return false;
+        }
     }
+            
 }
